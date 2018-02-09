55116adbfb3c31e7aedf1a5538f0304cc46516a5
Menno Finlay-Smits <inbox@menno.io>
2018-02-09 13:39:03 +1300

    Various style related fixes

--- cptv-download.py
+++ cptv-download.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
 from pathlib import Path
+import argparse
+import datetime
+import json
 import os
 
-import json
-import datetime
+from dateutil.parser import parse as parsedate
 
 from api import API
 from pool import Pool
-from dateutil.parser import parse as parsedate
 
-import argparse
+SPECIAL_DIRS = ['test', 'hard']
 
 
 class CPTVDownloader:
-
     def __init__(self):
         self.start_date = None
         self.end_date = None
@@ -51,7 +51,11 @@ class CPTVDownloader:
         api = API(url, self.user, self.password)
 
         print("Querying server {0}".format(url))
-        rows = api.query(limit=self.limit, startDate=self.start_date, endDate=self.end_date)
+        rows = api.query(
+            limit=self.limit,
+            startDate=self.start_date,
+            endDate=self.end_date,
+        )
 
         if self.auto_delete:
             self.update_file_locations()
@@ -68,7 +72,7 @@ class CPTVDownloader:
         """ Scans output folder building a list of all files. """
         self.file_list = {}
 
-        for root, subdirs, files in os.walk(self.out_folder):
+        for root, _, files in os.walk(self.out_folder):
             for file in files:
                 if file not in self.file_list:
                     self.file_list[file] = []
@@ -85,20 +89,15 @@ class CPTVDownloader:
             try:
                 tag_dir = get_tag_directory(r['Tags'])
 
-                out_dir = os.path.join(out_base, tag_dir)
+                out_dir = out_base / tag_dir
                 dt = parsedate(r['recordingDateTime'])
-                file_base = dt.strftime("%Y%m%d-%H%M%S") + "-" + r['Device']['devicename']
+                file_base = dt.strftime(
+                    "%Y%m%d-%H%M%S") + "-" + r['Device']['devicename']
 
-                path_base = os.path.join(out_dir, file_base)
+                path_base = str(out_dir / file_base)
 
                 if self.auto_delete:
-                    if file_base+'.cptv' in self.file_list:
-                        for existing_path in self.file_list[file_base+'.cptv']:
-                            if existing_path != out_dir and os.path.basename(existing_path) not in ['test','hard']:
-                                print("Found {} in {} but should be in {}".format(file_base, existing_path, out_dir))
-                                remove_file(os.path.join(existing_path, file_base + '.cptv'))
-                                remove_file(os.path.join(existing_path, file_base + '.dat'))
-                                remove_file(os.path.join(existing_path, file_base + '.mp4'))
+                    self._delete_existing(file_base, out_dir)
 
                 if tag_dir in self.ignore_tags:
                     continue
@@ -106,22 +105,32 @@ class CPTVDownloader:
                 if self.only_tags and tag_dir not in self.only_tags:
                     continue
 
-                if not os.path.exists(out_dir):
-                    os.makedirs(out_dir)
+                out_dir.mkdir(parents=True, exist_ok=True)
 
-                if iter_to_file(path_base + '.cptv', api.download_cptv(r['id'])):
+                if iter_to_file(path_base + '.cptv', api.download_cptv(
+                        r['id'])):
                     print(format_row(r) + '.cptv' + " [{}]".format(tag_dir))
 
                 if self.include_mp4:
-                    if iter_to_file(path_base + '.mp4', api.download_mp4(r['id'])):
+                    if iter_to_file(path_base + '.mp4',
+                                    api.download_mp4(r['id'])):
                         print(format_row(r) + '.mp4' + " [{}]".format(tag_dir))
 
                 if self.include_metadata:
-                    if not os.path.exists(path_base+'.txt'):
-                        json.dump(r, open(path_base+'.txt', 'w'), indent=4)
+                    if not os.path.exists(path_base + '.txt'):
+                        json.dump(r, open(path_base + '.txt', 'w'), indent=4)
 
             finally:
                 q.task_done()
+
+    def _delete_existing(self, file_base, new_dir):
+        for path in self.file_list.get(file_base + '.cptv', []):
+            path = Path(path)
+            if str(path) != new_dir and path.name not in SPECIAL_DIRS:
+                print("Found {} in {} but should be in {}".format(
+                    file_base, path, new_dir))
+                for ext in ['cptv', 'dat', 'mp4']:
+                    remove_file(str(path / (file_base + '.' + ext)))
 
 
 def remove_file(file):
@@ -147,23 +156,24 @@ def get_tag_directory(tags):
     for tag in tags:
         if tag.get('automatic', False):
             continue
-        tag_name = tag['animal'] if tag['event'] != 'false positive' else 'false-positive'
+        tag_name = tag[
+            'animal'] if tag['event'] != 'false positive' else 'false-positive'
         clip_tags.add(tag_name)
 
     if len(clip_tags) >= 2:
         return "multi"
 
-    if len(clip_tags) == 0:
+    if not clip_tags:
         return "untagged"
 
     tag = list(clip_tags)[0]
 
-    return tag.replace('/','-')
+    return tag.replace('/', '-')
 
 
 def format_row(row):
-    return "{} {} {}s".format(
-        row['id'], row['Device']['devicename'], row['duration'])
+    return "{} {} {}s".format(row['id'], row['Device']['devicename'],
+                              row['duration'])
 
 
 def iter_to_file(filename, source, overwrite=False):
@@ -176,31 +186,50 @@ def iter_to_file(filename, source, overwrite=False):
 
 
 def main():
-
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('out_folder', default=None, help='Root folder to place downloaded files in.')
-    parser.add_argument('user', default=None, help='Username')
-    parser.add_argument('password', default=None, help='Password')
-
-    parser.add_argument('-s', '--server', default=['https://api.cacophony.org.nz'],
-                        help='CPTV file server URL')
-    parser.add_argument('--start-date', default=None,
-                        help='If specified only files recorded on or after this date will be downloaded.')
-    parser.add_argument('--end-date', default=None,
-                        help='If specified only files recorded before or on this date will be downloaded.')
-    parser.add_argument('-r', '--recent', type=int, default=None,
-                        help='Download files only from the previous n days (overwrites start-date and end-date')
-
-    parser.add_argument('-t', '--tag', default=None,
-                        help='Specific tag to download, of not specified all non ignored tags will be downloaded')
-    parser.add_argument('-i', '--ignore', default=['untagged', 'multi'],
-                        help='List of tags to ignore')
-    parser.add_argument('-v', '--verbose', action='store_true', default=False,
-                        help='Print additional information')
-    parser.add_argument('-x', '--auto-delete', action="store_true", default=False,
-                        help='If enabled clips found in sub-folders other than their tag folder will be deleted.')
-    parser.add_argument('-l', '--limit', default=1000, help='Limit number of downloads')
+    # yapf: disable
+    parser.add_argument(
+        'out_folder',
+        help='Root folder to place downloaded files in.')
+    parser.add_argument('user', help='API server username')
+    parser.add_argument('password', help='API server password')
+    parser.add_argument(
+        '-s', '--server',
+        default=['https://api.cacophony.org.nz'],
+        help='CPTV file server URL')
+    parser.add_argument(
+        '--start-date',
+        help='If specified, only files recorded on or after this date will be downloaded.')
+    parser.add_argument(
+        '--end-date',
+        help='If specified, only files recorded before or on this date will be downloaded.')
+    parser.add_argument(
+        '-r', '--recent',
+        type=int,
+        help='Download files only from the previous n days (overwrites start-date and end-date')
+    parser.add_argument(
+        '-t', '--tag',
+        help='Specific tag to download, of not specified all non ignored tags will be downloaded')
+    parser.add_argument(
+        '-i', '--ignore',
+        default=['untagged', 'multi'],
+        help='List of tags to ignore')
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        default=False,
+        help='Print additional information')
+    parser.add_argument(
+        '-x', '--auto-delete',
+        action="store_true",
+        default=False,
+        help='If enabled clips found in sub-folders other than their tag folder will be deleted.')
+    parser.add_argument(
+        '-l', '--limit',
+        default=1000,
+        help='Limit number of downloads')
+    # yapf: enable
 
     args = parser.parse_args()
 
@@ -214,8 +243,10 @@ def main():
     downloader.end_date = args.end_date
 
     if args.recent:
-        print("Downloading new clips from the past {} days.".format(args.recent))
-        downloader.start_date = datetime.datetime.now() - datetime.timedelta(days=args.recent)
+        print("Downloading new clips from the past {} days.".format(
+            args.recent))
+        downloader.start_date = datetime.datetime.now() - datetime.timedelta(
+            days=args.recent)
         downloader.end_date = datetime.datetime.now()
 
     downloader.only_tags = args.tag
@@ -229,7 +260,8 @@ def main():
 
     server_list = []
     if args.server:
-        server_list = args.server if type(args.server) == list else [args.server]
+        server_list = args.server if isinstance(args.server, list) \
+                      else [args.server]
 
     for server in server_list:
         downloader.process(server)
