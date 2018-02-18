dc480dbf1ceda491535f129f18c01bda46a86307
Clare McLennan <clare@Clares-MacBook-Air-2.local>
2018-02-19 10:56:53 +1300

    Uploads now working

--- cptv-upload.py
+++ cptv-upload.py
@@ -1,18 +1,127 @@
 from deviceapi import DeviceAPI
 
+import argparse
+from pool import Pool
+
+import os
+
+import json
 
 class CPTVUploader:
     def __init__(self):
-        self.url = 'http://10.0.2.15:1080'
+        self.url = None
+        self.source_dir = None
 
     def process(self):
+        print('url is ', self.url)
         api = DeviceAPI(self.url)
+
+        files = self._find_files_to_upload()
+        for file in files:
+            self._uploadfile(api, file)
+
+        # pool = Pool(4, self._uploader, api)
+        # for file in files:
+        #     pool.put(file)
+        # pool.stop()
+
+
+    def _uploadfile(self, api, filename):
+        props = self._readPropertiesFromFile(filename)
+
+        print('props to transfer ')
+        print(props)
+
+        api.uploadrecording(filename, props)
+
+
+    def _uploader(self, queue, api):
+        while True:
+            
+            filename = queue.get()
+
+            if filename is None:
+                print('Closing thread')
+                break
+
+            try:
+                _uploadfile(api, filename)
+
+            finally:
+                queue.task_done()
+
+    def _find_files_to_upload(self):
+        cptvfiles = list()
+
+        print('testing dir', self.source_dir)
+        for root, dirs, files in os.walk(self.source_dir):
+            for file in files:
+                if file.lower().endswith(".cptv"):
+                    cptvfiles.append(os.path.join(root, file))
+        
+        return cptvfiles
+
+    def _readPropertiesFromFile(self, filename): 
+        basefile = os.path.splitext(filename)[0]
+        jsonfilename = basefile + '.txt'
+
+        print(jsonfilename)
+
+        if (os.path.isfile(jsonfilename)):
+            with open(jsonfilename, 'r') as propsfile: 
+
+                oldprops = json.load(propsfile)
+
+                # List of properties to transfer.   Many such as Id, we don't want to transfer. 
+                propTypesToTransfer = ("batteryCharging", "additionalMetadata", "comment", "location", 
+                            "fileSize", "batteryLevel", "duration", "rawFileSize", "airplaneModeOn", 
+                            "version", "recordingDateTime", "fileMimeType", "type")
+
+                newProps = dict()
+
+                for key in propTypesToTransfer: 
+                    if (oldprops[key] != None):
+                        newProps[key] = oldprops[key]
+                
+                # Tags can be imported at the moment - maybe because there is no tagger Id. 
+                # tags = oldprops['Tags']
+
+                # if (tags != None):
+                #     tagPropTypesToTransfer = ("confidence", "number", "sex", "updatedAt", "startTime", 
+                #         "age","automatic", "createdAt", "trapType", "event", "animal", "duration")
+                #     newTags = list()
+
+                #     for tag in tags:
+                #         newTagProps = dict()
+                #         for key in tagPropTypesToTransfer: 
+                #             if (tag[key] != None):
+                #                 newTagProps[key] = tag[key]
+
+                #         newTags += newTagProps
+                    
+                #     newProps['Tags'] = newTagProps
+
+                return json.dumps(newProps)
+        
+        return None
 
 
 def main(): 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('server_url',  help='Server (base) url to send the CPTV files to')
+    
+    parser.add_argument('source_dir',  help='Root folder where files for upload are stored')
+    
     uploader = CPTVUploader()
 
+    args = parser.parse_args()
+
+    uploader.url = args.server_url
+    uploader.source_dir = args.source_dir
+
     uploader.process()
+
 
 
 if __name__ == '__main__':
--- deviceapi.py
+++ deviceapi.py
@@ -1,7 +1,8 @@
+import os
 import json
 import requests
 from urllib.parse import urljoin
-
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 class DeviceAPI:
 
@@ -21,8 +22,36 @@ class DeviceAPI:
 
         r.raise_for_status()
 
-
-        print(r.json().get('token'))
-        
         return r.json().get('token')
+
+    def uploadrecording(self, filename, jsonProps = None): 
+        url = urljoin(self._baseurl, '/api/v1/recordings')
+        print(filename)
+
+        if (jsonProps == None):
+            jsonProps = '{"type": "thermalRaw"}'
+            print (' null props')
+
+        with open(filename, 'rb') as thermalfile: 
+            multipart_data = MultipartEncoder(
+                fields={
+                        # a file upload field
+                        'file': ('file.py', thermalfile),
+                        # plain text fields
+                        'data': jsonProps 
+                    }
+                )
+            headers={'Content-Type': multipart_data.content_type, 'Authorization': self._token}
+            r = requests.post(url, data=multipart_data, headers=headers)
+
+
+        if r.status_code == 400:
+            messages = r.json()['messages']
+            print ("request failed ({}): {}".format(r.status_code, messages))
+        else:
+            r.raise_for_status()
+
+
+
+    
 
