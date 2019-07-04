54d7f495e374652451f7ee34beb98c45a782ebac
GP <g.ferraro22@gmail.com>
2019-07-04 16:36:06 +1200

    updated cptv-reprocess

--- cptv-reprocess.py
+++ cptv-reprocess.py
@@ -5,48 +5,39 @@ import argparse
 from api import API
 
 
-class CPTVReprocessor:
-    def __init__(self, server_url, user, password, id, limit):
-        self.user = user
-        self.password = password
-        self.url = server_url
-        self.recording_id = id
-        self.limit = limit
+def reprocess(server_url, user, password, recording_id, limit):
+    """ Reprocesses all specified recording from specified server """
 
-    def reprocess(self):
-        """ Reprocesses all specified recording from specified server """
+    api = API(server_url, user, password)
 
-        api = API(self.url, self.user, self.password)
+    print(f"Querying server {server_url}")
+    print(f"Limit is {limit}")
+    if recording_id:
+        print(f"Recording id is {recording_id}")
+        api.reprocess([recording_id])
+        return
 
-        print(f"Querying server {self.url}")
-        print(f"Limit is {self.limit}")
-        if self.recording_id:
-            print(f"Recording id is {self.recording_id}")
-            api.reprocess([self.recording_id])
-            return
+    where = {
+        "additionalMetadata.algorithm": {"$eq": None},
+        "type": "thermalRaw",
+        "processingState": {"$ne": "FINISHED"},
+    }
 
-        where = {}
-        # algorithm was only added when trackbased tagging was introduced
-        where["additionalMetadata.algorithm"] = {"$eq": None}
-        where["type"] = "video"
-        count = api.query(where=where, limit=self.limit, raw_json=True)["count"]
-        if count:
-            print(f"Still reprocessing {count} records")
-            return
+    count = api.query(where=where, limit=limit, raw_json=True)["count"]
+    if count:
+        print(f"Still reprocessing {count} records")
+        return
 
-        where["processingState"] = "FINISHED"
-        rows = api.query(where=where, limit=self.limit)
-        recording_ids = [row["id"] for row in rows]
-        print(f"Reprocessing recording ids: {recording_ids}")
-        api.reprocess(recording_ids)
+    where["processingState"] = "FINISHED"
+    rows = api.query(where=where, limit=limit)
+    recording_ids = [row["id"] for row in rows]
+    print(f"Reprocessing recording ids: {recording_ids}")
+    api.reprocess(recording_ids)
 
 
 def main():
     args = parse_args()
-    reprocessor = CPTVReprocessor(
-        args.server, args.user, args.password, args.recording_id, args.limit
-    )
-    reprocessor.reprocess()
+    reprocess(args.server, args.user, args.password, args.recording_id, args.limit)
 
 
 def parse_args():
@@ -58,12 +49,13 @@ def parse_args():
         "-s",
         "--server",
         default=["https://api.cacophony.org.nz"],
-        help="CPTV file server URL",
+        help="API server URL",
     )
     parser.add_argument(
         "-l",
         "--limit",
-        default=None,
+        type=int,
+        default=100,
         help="Number of recordings to set to reprocessing",
     )
     parser.add_argument(
