d355b274a17dbf94d6f5d068811a3c66a54caea7
cam <cameron.ryan.pears@gmail.com>
2019-12-09 11:54:39 +1300

    Clean up args

--- cptv-upload.py
+++ cptv-upload.py
@@ -14,24 +14,21 @@ def main():
     parser.add_argument("password", help="Password to authenticate with")
     parser.add_argument("groupname", help="Group name to upload on behalf of")
     parser.add_argument("devicename", help="Device to upload on behalf of")
-    parser.add_argument("--filename", help="File to upload")
-    parser.add_argument("--filedir", help="Will upload all cptv files in this dir")
+    parser.add_argument("filename", help="File to upload. If it is a directory will upload all cptv files in that dir")
 
     args = parser.parse_args()
 
     api = API(args.server_url, args.username, args.password)
-    if args.filename != None:
-        print("uploading just one recording")
-        api.upload_recording(args.groupname, args.devicename, args.filename)
-    elif args.filedir != None:
+    if os.path.isdir(args.filename):
         print("uploading multiple files")
-        for file in os.listdir(args.filedir):
+        for file in os.listdir(args.filename):
             if file.endswith(".cptv"):
-                filepath = os.path.join(args.filedir, file)
+                filepath = os.path.join(args.filename, file)
                 print(filepath)
                 api.upload_recording(args.groupname, args.devicename, filepath)
     else:
-        print("filename or filedor is required")
+        print("uploading just one recording")
+        api.upload_recording(args.groupname, args.devicename, args.filename)
 
 if __name__ == "__main__":
     main()
