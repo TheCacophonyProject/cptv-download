1a97d4600347cc137c99f3b5731e450dfd9599d1
gferraro <g.ferraro22@gmail.com>
2025-10-07 16:16:58 +1300

    re added tests

---
+++ tests/test_uploadanddownload.py
@@ -0,0 +1,52 @@
+import pytest
+import os
+import tempfile
+import subprocess
+import shutil
+
+from cacophonyapi.user import UserAPI
+
+
+class TestUploadDownload:
+    def test_upload(self, test_config):
+        os.chdir("../")
+        file_names = os.listdir("tests/test_data")
+        for file_name in file_names:
+            result = subprocess.run(
+                [
+                    "python3",
+                    "cptv-upload.py",
+                    test_config.api_url,
+                    test_config.admin_email,
+                    test_config.admin_password,
+                    "test-group",
+                    "test-device",
+                    "tests/test_data/{0}".format(file_name),
+                ]
+            )
+            assert result.returncode == 0
+
+    def test_download(self, test_config):
+        if os.path.exists("downloads1"):
+            shutil.rmtree("downloads1")
+
+        os.mkdir("downloads1")
+        result = subprocess.run(
+            [
+                "python3",
+                "cptv-download.py",
+                "-i",
+                "None",
+                "-m",
+                "any",
+                "-s",
+                test_config.api_url,
+                "downloads1",
+                test_config.admin_email,
+                test_config.admin_password,
+            ]
+        )
+
+        shutil.rmtree("downloads1")
+
+        assert result.returncode == 0
